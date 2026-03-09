98a76c295457d04a2a80a7ad47dfb6f4e4ddfc27
YUTIAN BAI <yutian_bai@outlook.com>
2026-03-08 22:17:47 -0700

    filechange

--- FormTest/src/main.tsx
+++ FormTest/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/App";
+import App from "./app/App.tsx";
 import "./styles/index.css";
 
 class ErrorBoundary extends React.Component<
